93e4b1d7739238d0b043e145bb540d3c3c46ee82
yonatan-E <yonatan16.er@gmail.com>
2021-03-29 00:38:57 +0300

    Adding a basic flight gear controller interface

---
+++ ex1/Model/IFlightGearModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Net.Sockets;
+
+namespace ex1.Model
+{
+    interface IFlightGearModel : INotifyPropertyChanged
+    {
+        public TcpClient TCPClient { get; set; }
+
+        // need to add properties for the details of the flight, such as height, speed, etc.
+
+        public void RenderDetails(string details);
+    }
+}
