b63bd0a1f3660fa20eccd5bd31cdcf696549a2c0
yonatan-E <yonatan16.er@gmail.com>
2021-04-13 18:20:06 +0300

    Making ValueConverters folder

--- ex1/View/ValueConverters/ControllerValuesConverter.cs
+++ ex1/View/ValueConverters/ControllerValuesConverter.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using System.Windows.Data;
 using System.Windows;
 
-namespace ex1.View
+namespace ex1.View.ValueConverters
 {
     class ControllerValuesConverter : IMultiValueConverter
     {
