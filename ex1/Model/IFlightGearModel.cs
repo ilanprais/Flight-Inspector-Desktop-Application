13629517495e04ea6a0037f99b5dcc5b8890ee53
AEfrati8 <58906555+AEfrati8@users.noreply.github.com>
2021-04-14 21:15:06 +0300

    Update IFlightGearModel.cs

--- ex1/Model/IFlightGearModel.cs
+++ ex1/Model/IFlightGearModel.cs
@@ -6,6 +6,7 @@ namespace ex1.Model
 {
     public interface IFlightGearModel : INotifyPropertyChanged
     {
+            //Properties
         public string FlightDataFilePath { get; set; }
 
         public List<Frame> Frames { get; }
@@ -18,13 +19,21 @@ namespace ex1.Model
         public bool RenderingStopped { get; set; }
         public int FrameRate { get; }
 
+        //Connects to the flightgear
         public Task ConnectToFG(string ip, int port);
+        
+                //Disconnects from the flightgear
         public Task DisconnectFromFG();
+                //Renders the model
         public Task Render();
 
+        //Detects if there was an anomaly
         public void DetectAnomaly();
 
+        //Finds the most corelative variable
         public RandomVariable FindMostCorelative(RandomVariable variable, List<RandomVariable> variables);
+        
+                //Calculates the linear regression
         public (double, double) LinearRegression(RandomVariable first, RandomVariable second);
     }
 }
